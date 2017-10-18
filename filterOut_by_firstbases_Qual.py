84c5b2ce825f49196f46feb75f9040327a4e4b7e
Marcela <marcela.uliano@gmail.com>
2017-10-18 15:43:27 +0200

    Update filterOut_by_firstbases_Qual.py

--- filterOut_by_firstbases_Qual.py
+++ filterOut_by_firstbases_Qual.py
@@ -1,12 +1,21 @@
 #Learning process - this script filter out complete fastq sequences that have phred qualities bellow 14.
-#By Marcela Uliano-Silva tutored by Felix Heeger, 2017.
+#By Marcela Uliano-Silva tutored by Felix Heeger and Max Driller, 2017.
 
 from Bio import SeqIO
+import gzip
+
+
+mainfile = "example.fastq"
+if mainfile.endswith(".gz"):
+	my_fastq_parse = gzip.open(mainfile)
+else:
+	my_fastq_parse = open(mainfile)
 output = open("out.txt", "w")
-for record in SeqIO.parse(open("example.fastq"), "fastq"):
+
+for record in SeqIO.parse(my_fastq_parse, "fastq"):
 	flag = True 
 	for qual in record.letter_annotations["phred_quality"][0:4]:
-		if qual <= 14:
+		if qual <= 22:
 			flag = False
 	if flag == True:
 		#print(record.format("fastq"))
