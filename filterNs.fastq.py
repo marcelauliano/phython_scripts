0965899c20b119a63fd0334e2e4852b799d3275d
Marcela <marcela.uliano@gmail.com>
2017-10-26 17:49:12 +0200

    Update filterNs.fastq.py

--- filterNs.fastq.py
+++ filterNs.fastq.py
@@ -5,14 +5,16 @@ from Bio import SeqIO
 import gzip
 import sys
 
-mainfile = sys.argv[1]
-if mainfile.endswith(".gz"):
-	my_fastq_parse = gzip.open(mainfile)
+input_file = sys.argv[1]
+#input_file is a variable. Line 8 states that something will be saved in the variable input_file. The something is the argv[1]. The sys is an object, and the argv that comes after the . is part of that object and its a list, and the [] allow you to access one of the itens on that list. In this case, the input, in argv, 0 prints the name of the script. 
+if input_file.endswith(".gz"):
+	my_fastq_parse = gzip.open(input_file)
 else:
-	my_fastq_parse = open(mainfile)
+	my_fastq_parse = open(input_file)
 output = open(sys.argv[2], "w")
 
 for record in SeqIO.parse(my_fastq_parse, "fastq"):
+	#record is just a variable name
 	if record.seq.count('N') == 0:
 		output.write(record.format("fastq"))
 output.close()
