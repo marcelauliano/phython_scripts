ecf2eed06651d48dfb9092586cb951891aafa57e
Marcela <marcela.uliano@gmail.com>
2017-10-27 15:58:45 +0200

    Update filterNs.fastq.py

--- filterNs.fastq.py
+++ filterNs.fastq.py
@@ -5,16 +5,29 @@ from Bio import SeqIO
 import gzip
 import sys
 
-input_file = sys.argv[1]
-#input_file is a variable. Line 8 states that something will be saved in the variable input_file. The something is the argv[1]. The sys is an object, and the argv that comes after the . is part of that object and its a list, and the [] allow you to access one of the itens on that list. In this case, the input, in argv, 0 prints the name of the script. 
-if input_file.endswith(".gz"):
-	my_fastq_parse = gzip.open(input_file)
+input_file_R1 = sys.argv[1]
+input_file_R2 = sys.argv[2]
+#input_file is a variable. Line 8 states that something will be saved in the variable input_file. The something is the argv[1]. The sys is an object, and the argv (argument) that comes after the . is part of that object and its a list, and the [] allow you to access one of the itens on that list. In this case, the input, in argv, 0 prints the name of the script. 
+if input_file_R1.endswith(".gz"):
+	my_fastq_parse_R1 = gzip.open(input_file_R1)
 else:
-	my_fastq_parse = open(input_file)
-output = open(sys.argv[2], "w")
+	my_fastq_parse_R1 = open(input_file_R1)
+if input_file_R2.endswith(".gz"):
+	my_fastq_parse_R2 = gzip.open(input_file_R2)
+else:
+	my_fastq_parse_R2 = open(input_file_R2)
+output_file_R1 = open(sys.argv[3], "w")
+output_file_R2 = open(sys.argv[4], "w")
 
-for record in SeqIO.parse(my_fastq_parse, "fastq"):
+fortheSecondToo = SeqIO.parse(my_fastq_parse_R2, "fastq")
+for record_forThe_R1 in SeqIO.parse(my_fastq_parse_R1, "fastq"):
 	#record is just a variable name
-	if record.seq.count('N') == 0:
-		output.write(record.format("fastq"))
-output.close()
+	record_forThe_R2 = next(fortheSecondToo)
+	if record_forThe_R1.seq.count('N') == 0 and record_forThe_R2.seq.count('N') == 0: 
+		output_file_R1.write(record_forThe_R1.format("fastq"))
+		output_file_R2.write(record_forThe_R2.format("fastq"))
+output_file_R1.close()
+output_file_R2.close()
+
+
+
