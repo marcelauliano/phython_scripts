90584e98bf596b4d9365d80be8be07216a117e6a
Marcela <marcela.uliano@gmail.com>
2018-11-19 16:12:05 +0100

    Update Ns.py

--- Ns.py
+++ Ns.py
@@ -1,13 +1,16 @@
 #very quick write.
-
 from __future__ import division
 from Bio import SeqIO
-Ns = open('contentNs.txt', 'w')
-for sequence in SeqIO.parse('/home/muliano/VGP-running/pri.asm.20180817.fasta-buscos0', 'fasta'):
+import sys
+input = sys.argv[1]
+inputopen = open(input)
+Ns = open(sys.argv[2], 'w')
+for sequence in SeqIO.parse(inputopen, 'fasta'):
         id = sequence.id
         count1 = len(sequence)
         countNs = sequence.seq.upper().count('N')
         out = id + ' ' + str(countNs) + '\n'
         Ns.write(out)
 Ns.close()
+
         
