4ec4e8eb6523a7a694e34fea47e511834e2ac4a6
Marcela <marcela.uliano@gmail.com>
2018-11-23 14:17:36 +0100

    Create splitmultifastaOutByID.py

---
+++ splitmultifastaOutByID.py
@@ -0,0 +1,11 @@
+#spliting a multifasta file in individual fasta files having the ID and file name
+from __future__ import division
+from Bio import SeqIO
+import sys
+input = sys.argv[1]
+inputopen = open(input)
+for sequence in SeqIO.parse(inputopen, 'fasta'):
+        id = sequence.id
+        #out = sequence.format('fasta')
+        with open(id, 'w') as output:
+                output.write(sequence.format('fasta'))
