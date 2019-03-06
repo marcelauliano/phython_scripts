b976ca01d1c901fa99722c04f4e347303dbfe43c
Marcela <marcela.uliano@gmail.com>
2019-03-06 10:10:49 +0100

    Create cutfastBycoords.py

---
+++ cutfastBycoords.py
@@ -0,0 +1,13 @@
+#lazs without args
+
+from __future__ import division
+from Bio import SeqIO
+output = open('CD.bionano.length', 'w')
+for sequence in SeqIO.parse('/home/muliano/xenarthra/c.didactylus/bionano/CD.bionano.fasta', 'fasta'):
+        id = sequence.id
+        get = sequence[12477:28968]
+        length = len(get)
+        out = '> ' + 'length got is ' + str(length) + '\n' + get.format('fasta')
+        output.write(out)
+output.close()
+#       print(str(length) +  get.format('fasta'))
