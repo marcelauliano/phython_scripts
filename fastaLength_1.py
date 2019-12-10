83ff84b8c66be3ea5155dbfb99b6ddb1e7ff9535
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2019-12-10 14:19:57 +0000

    Update fastaLength_1.py

--- fastaLength_1.py
+++ fastaLength_1.py
@@ -3,7 +3,6 @@ from Bio import SeqIO
 import sys
 input_fasta = sys.argv[1]
 output_file = open(sys.argv[2], "w")
-#length = open('mChoDid1.pri.asm.20190228.fasta.length', 'w')
 for sequence in SeqIO.parse(input_fasta, 'fasta'):
         id = sequence.id
         count = len(sequence)
