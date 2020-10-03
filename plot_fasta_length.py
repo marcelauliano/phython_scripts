9d19d88555ee54c13f764f5715c822fb3c99e29e
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2020-10-03 17:29:01 +0100

    Update plot_fasta_length.py

--- plot_fasta_length.py
+++ plot_fasta_length.py
@@ -1,9 +1,16 @@
 from Bio import SeqIO
 import pylab
 import sys
+import gzip
 input = sys.argv[1]
 output = open(sys.argv[2], "wb")
-sizes = [len(rec) for rec in SeqIO.parse(input, "fasta")]
+if input.endswith(".gz"):
+        my_fasta_parse = gzip.open(input, "rt")
+else:
+        my_fasta_parse = open(input)
+
+
+sizes = [len(rec) for rec in SeqIO.parse(my_fasta_parse, "fasta")]
 pylab.hist(sizes, bins=20)
 pylab.title(
     "%i  sequences\nLengths %i to %i" % (len(sizes), min(sizes), max(sizes))
