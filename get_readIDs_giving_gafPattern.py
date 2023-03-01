30dd7fac561775e03f354acdf5e0f2f3503ddcdb
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2023-03-01 15:47:52 +0000

    Update get_readIDs_giving_gafPattern.py

--- get_readIDs_giving_gafPattern.py
+++ get_readIDs_giving_gafPattern.py
@@ -8,8 +8,7 @@ parser.add_argument("-h", "--help", action="help", default=argparse.SUPPRESS, he
 parser.add_argument("-g", help= "-g: gaf file output of Graphaligner", required = "True")
 parser.add_argument("-p", help= "-p: pattern to search on patch_matching field", required = "True")
 args = parser.parse_args()
-m_names=('queryname','query_length','query_start','query_end','strand', 'path_matching','path_length','path_start','path_end', 'residue_matches', 'alignm_length', 'mapping_qual', 'other1', 
-'other2', 'other3', 'identity', 'other4')
+m_names=('queryname','query_length','query_start','query_end','strand', 'path_matching','path_length','path_start','path_end', 'residue_matches', 'alignm_length', 'mapping_qual', 'other1', 'other2', 'other3', 'identity', 'other4')
 mytypes={'mapping_qual':'Int64'}
 df4=pd.read_csv(args.g, names=m_names,  sep="\t", dtype=mytypes)
 df5=df4[df4['path_matching' ]== args.p]
