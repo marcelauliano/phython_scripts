12b9ced673b5de78b7d1f65b05570ff6ee7eb1ee
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2023-03-03 10:51:21 +0000

    Update lines_into_commaSep.py

--- lines_into_commaSep.py
+++ lines_into_commaSep.py
@@ -1,12 +1,10 @@
 import sys
 input_file = sys.argv[1]
-output = open(sys.argv[2], "w")
+output_file = open(sys.argv[2], "w")
 
 with open(input_file, "r") as input_file:
     lines = input_file.readlines()
 
 # Remove newline characters from each line
 lines = [line.strip() for line in lines]
-
-with open(output, "w") as output_file:
-    output_file.write(",".join(lines))
+output_file.write(",".join(lines))
